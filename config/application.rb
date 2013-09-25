c176d4ca7fa34103919e591fac22ffcce62d6985
xiongbo <xiongbo027@gmail.com>
2013-09-25 11:56:57 +0800

    replace rspec with test

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require "active_record/railtie"
-require "active_controller/railtie"
-require "rails/test_unit/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
