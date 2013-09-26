0ba828bea9b59489f069a98ce045649559027b8c
xiongbo <xiongbo027@gmail.com>
2013-09-26 17:12:32 +0800

    update ENGINES

--- lib/analysis_helper/search_engine.rb
+++ lib/analysis_helper/search_engine.rb
@@ -1,16 +1,16 @@
 module AnalysisHelper
   ENGINES = [
               ["google", "utf8", "q"],
-              ["baidu", "utf8", "q"],
-              ["yahoo", "utf8", "q"],
-              ["bin", "utf8", "q"],
+              ["baidu", "gb2312", "(?:wd|word)"],
+              ["yahoo", "utf8", "p"],
+              ["bing", "utf8", "q"],
               ["youdao", "utf8", "q"],
               ["live", "utf8", "q"],
               ["360", "utf8", "q"],
-              ["soso", "utf8", "q"],
-              ["163", "utf8", "q"],
-              ["sogou", "utf8", "q"],
-              ["iask", "utf8", "q"],
+              ["soso", "utf8", "(?:w|key)"],
+              ["163", "gb2312", "q"],
+              ["sogou", "gb2312", "query"],
+              ["iask", "gb2312", "q"],
               ["lycos", "utf8", "q"]
             ]
 
